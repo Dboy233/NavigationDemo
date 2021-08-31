927d151f081e39175a58b5a02b496fb219ef722d
Dboy <894230813@qq.com>
2021-08-31 14:19:20 +0800

    添加一个优化后的反射工具类

--- app/src/main/java/com/dboy/navigation/base/BaseFragment.kt
+++ app/src/main/java/com/dboy/navigation/base/BaseFragment.kt
@@ -10,10 +10,7 @@ import android.view.animation.AnimationUtils
 import androidx.core.view.OneShotPreDrawListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
-import java.lang.reflect.ParameterizedType
-import java.util.*
+import com.dboy.navigation.utils.BindingReflex
 
 /**
  *   @author DBoy
@@ -23,7 +20,7 @@ import java.util.*
 abstract class BaseFragment<VM : ViewModel> : Fragment() {
 
     protected val viewModel: VM by lazy {
-        reflexViewModel(javaClass, this)
+        BindingReflex.reflexViewModel(javaClass, this)
     }
 
     /**
@@ -89,29 +86,6 @@ abstract class BaseFragment<VM : ViewModel> : Fragment() {
     override fun setInitialSavedState(state: SavedState?) {
         super.setInitialSavedState(state)
         Log.d(TAG_LIFE, "${javaClass.simpleName}  setInitialSavedState: $state")
-    }
-
-    /**
-     * 使用反射初始化ViewModel,如果比较喜欢这个方式的话请参照
-     * https://github.com/Quyunshuo/AndroidBaseFrameMVVM/blob/master/Lib_Base/src/main/java/com/quyunshuo/base/utils/BindingReflex.kt
-     * 这个也是我参与完成的，有一整套的反射方式。
-     */
-    open fun <VM : ViewModel> reflexViewModel(aClass: Class<*>?, owner: ViewModelStoreOwner): VM {
-        try {
-            val actualTypeArguments = (Objects.requireNonNull(
-                aClass?.genericSuperclass
-            ) as ParameterizedType).actualTypeArguments
-            for (i in actualTypeArguments.indices) {
-                val tClass: Class<Any> = actualTypeArguments[i] as Class<Any>
-                if (ViewModel::class.java.isAssignableFrom(tClass)) {
-                    return ViewModelProvider(owner)[tClass as Class<VM>]
-                }
-            }
-            return reflexViewModel<VM>(aClass?.superclass, owner)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        throw RuntimeException("失败")
     }
 
 
